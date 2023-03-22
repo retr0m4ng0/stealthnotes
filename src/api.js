f7df8fde930bfd3e59791e7b1c1d253df1eaea24
retr0m4ng0 <128650977+retr0m4ng0@users.noreply.github.com>
2023-03-23 00:29:06 +0100

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,143 @@
+const Router = require('express').Router
+const mongodb = require('mongodb');
+const crypto = require('./lib/crypto');
+
+module.exports = (db) => {
+  const api = Router();
+
+  api.get('/', (_req, res) => {
+  	res.render('home', {
+        deleteStaleMessages: process.env.DELETE_STALE_MESSSAGES == 'true' ? true : false,
+        deleteStaleMessagesDays: +process.env.DELETE_STALE_MESSAGES_DAYS,
+    });
+  });
+
+  api.post('/api/create', async (req, res) => {
+    try {
+      const { iv, securityKey, ttl, note } = req.body;
+      if (!securityKey) {
+        securityKey = crypto.generateIVAndSecurityKey().securityKey;
+      }
+      if (!iv) {
+        iv = crypto.generateIVAndSecurityKey().iv;
+      }
+      const encryptedNote = crypto.encrypt(note, iv, securityKey);
+      const url = (process.env.PUBLIC_URL || '') + `/api/read/${securityKey}/${iv}`;
+      const timestamp = new Date().getTime();
+      await db.collection('notes').insertOne({note: encryptedNote, securityKey, ttl, timestamp});
+      res.json({url});
+    } catch (error) {
+      console.error(error);
+      res.json({error: 'Error creating note, please try again later.'});
+    }
+  });
+
+  api.get('/api/read/:securityKey/:iv', async (req, res) => {
+    const { securityKey, iv } = req.params;
+    try {
+      const encryptedNote = await db.collection('notes').findOne({securityKey});
+      try {
+        const decryptedNote = crypto.decrypt(encryptedNote.note, iv, securityKey);
+        const ascii = crypto.isASCII(iv + securityKey);
+        await db.collection('notes').deleteOne({securityKey});
+        if (ascii) {
+          res.json({note: decryptedNote, error: 'Note has been destroyed.'});
+        } else {
+          res.json({note: null, error: 'Incorrect URL parameters. Note has been destroyed.'})
+        }
+      } catch (error) {
+        console.error(error);
+        await db.collection('notes').deleteOne({securityKey});
+        res.json({note: null, error: 'Incorrect key. Note has been destroyed.'})
+      }
+      
+    } catch (error) {
+      console.error(error);
+      res.json({note: null, error: 'Could not find note.'});
+    }
+    
+  });
+
+  api.post('/create', async (req, res) => {
+    try {
+      const { note, ttl, securityKey } = req.body;
+      const timestamp = new Date().getTime();
+      await db.collection('notes').insertOne({note, securityKey, ttl, timestamp});
+      res.json();
+    } catch (error) {
+      console.error(error);
+      res.json({error: 'Error creating note, please try again later.'});
+    }
+  });
+
+  api.get('/read/:securityKey/:iv', async (req, res) => {
+    const { securityKey, iv } = req.params;
+    try {
+      const encryptedNote = await db.collection('notes').findOne({securityKey});
+      if (!encryptedNote) {
+        res.render('read', {note: null, error: 'Could not find note.'});
+      } else {
+        if (encryptedNote.ttl == 0) { // non-timed ttl
+          try {
+            crypto.decrypt(encryptedNote.note, iv, securityKey);
+            const destroyUrl = `/read/${securityKey}/${iv}/destroy`;
+            res.render('areyousure', {destroyUrl});
+          } catch (error) {
+            console.error(error);
+            await db.collection('notes').deleteOne({securityKey});
+            res.render('read', {note: null, error: 'Incorrect key. Note has been destroyed.'})
+          }
+        } else { // timed note
+          const currentTimestamp = new Date().getTime();
+          if (currentTimestamp >= ((encryptedNote.ttl * 1000) + encryptedNote.timestamp)) { // Expired
+            await db.collection('notes').deleteOne({securityKey});
+            res.render('read', {note: null, error: 'The note you want to read had a time limit and it has expired'});
+          } else {
+            try {
+              // check to see if note can even be decrypted, but don't send back
+              res.render('read', {note: encryptedNote.note});
+            } catch (error) {
+              console.error(error);
+              await db.collection('notes').deleteOne({securityKey});
+              res.render('read', {note: null, error: 'Incorrect key. Note has been destroyed.'})
+            }
+          }
+        }
+      }
+      
+    } catch (error) {
+      console.error(error);
+      res.render('read', {error: 'An error occurred when retrieving note'});
+    }
+  });
+
+  api.get('/read/:securityKey/:iv/destroy', async (req, res) => {
+    const { securityKey, iv } = req.params;
+    try {
+      const encryptedNote = await db.collection('notes').findOne({securityKey});
+      if (!encryptedNote) {
+        res.render('read', {note: null, error: 'Could not find note.'});
+      } else {
+        try {
+          const ascii = crypto.isASCII(securityKey + iv);
+          await db.collection('notes').deleteOne({securityKey});
+          if (ascii) {
+            res.render('read', {note: encryptedNote.note, error: 'Note has been destroyed.'});
+          } else {
+            res.render('read', {note: null, error: 'Incorrect URL parameters. Note has been destroyed.'})
+          }
+        } catch (error) {
+          console.error(error);
+          await db.collection('notes').deleteOne({securityKey});
+          res.render('read', {note: null, error: 'Incorrect key. Note has been destroyed.'})
+        }
+        
+      }
+    } catch (error) {
+      console.error(error);
+      res.render('read', {error: 'An error occurred when retrieving note'});
+    }
+  });
+
+  return api;
+}
---
+++ src/index.js
@@ -0,0 +1,46 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const mongodb = require('mongodb');
+const { engine } = require('express-handlebars');
+const scheduler = require('node-schedule');
+const api = require('./api');
+const MongoClient = mongodb.MongoClient;
+
+const app = express();
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || '0.0.0.0';
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.static('src/public'));
+
+app.engine('handlebars', engine({ defaultLayout: 'main', layoutsDir: `${__dirname}/views/layouts` }));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/views`);
+
+async function createScheduler(db) {
+    scheduler.scheduleJob('0 2 * * *', async () => {
+        try {
+            await db.collection('notes').deleteMany({timestamp: {$lt: getDaysPastDate(+process.env.DELETE_STALE_MESSAGES_DAYS)}})
+        } catch (error) {
+            console.error('Unable to fetch notes for automated scheduler', error);
+        }
+    });
+}
+
+MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) return console.error(err);
+    app.listen(port, host, () => {
+        console.log(`Listening on port http://localhost:${port}`);
+        const db = client.db(process.env.MONGO_DB);
+        app.use(api(db));
+        app.use((req, res) => {
+            res.render('404');
+        });
+        if (process.env.DELETE_STALE_MESSSAGES == 'true') {
+            createScheduler(db);
+        }
+    });
+});
